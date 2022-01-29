47eb39a72c867f529a2ae2a1744d2ef7c33782dd
unknown <aniketkhote99@gmail.com>
2022-01-29 23:02:14 +0530

    tailwindcss installed

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'body':'#17171F',
+        'selected-text':'#A3A3FF',
+        'theme':'#3F3FFF',
+        'nav':'#404053',
+        'secondary':'#9191A4',
+        'badge':'#3F3F51',
+        'input-border':'#565666',
+        'input':'#2A2A35'
+      },
+      fontFamaily:{
+        'poppins':["'Poppins'",'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
